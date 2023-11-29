5f9361ea91874dfc2a931966fbfdf78abf80a842
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2023-11-29 17:55:48 -0300

    add annotation and controller

--- src/main/java/br/com/springboot/SpringbootFundamentalsApplication.java
+++ src/main/java/br/com/springboot/SpringbootFundamentalsApplication.java
@@ -2,8 +2,10 @@ package br.com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.springboot")
 public class SpringbootFundamentalsApplication {
 
 	public static void main(String[] args) {
